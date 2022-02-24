a5a3dcd78adf0a562d210c5bc38c97e049c3f8c6
Solo21-12 <93918988+solo21-12@users.noreply.github.com>
2022-02-24 17:27:10 +0300

    my new gitignore file

---
+++ 0x01-git/.gitignore/new.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+double add(double, double);
+double sub(double, double); 
+double mul(double, double); 
+double div(double, double);
+double result;
+int main(){
+    double num1,num2;
+    char zx;
+    cout<<"enter the number ";cin>>num1>>num2;
+    cout<<"choose the operation ";cin>>zx;
+    cout<<"the result of your operation is ";
+    if(zx=='+'){
+        add(num1,num2);
+        
+    }
+    else if(zx=='-'){
+        sub(num1,num2);
+         
+    }
+    else if(zx=='*'){
+        mul(num1,num2);
+        
+    }
+    else 
+    {
+        div(num1,num2);
+}
+}
+double add(double num1, double num2){
+    result=num1+num2;
+    cout<<result;
+    return 0;
+}
+double sub(double num1, double num2){
+    result=num1-num2;
+    cout<<result;
+    return 0;
+
+}
+double mul(double num1, double num2){
+    result=num1*num2;
+    cout<<result;
+    return 0;
+   
+}
+double div(double num1, double num2){
